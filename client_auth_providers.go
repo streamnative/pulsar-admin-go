3ce9b341b6fc68554fe0e3e836fae2fb6c6dccd7
Andy Walker <andy@andy.dev>
2023-07-27 01:40:58 -0400

    WIP: Reorg

---
+++ client_auth_providers.go
@@ -0,0 +1,124 @@
+package pulsaradmin
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/streamnative/pulsar-admin-go/internal/auth"
+)
+
+// The following are the valid values for pluginName in calls to [AuthProviderPlugin]
+const (
+	AuthPluginTLS         = "tls"
+	AuthPluginClassTLS    = "org.apache.pulsar.client.impl.auth.AuthenticationTls"
+	AuthPluginOAuth2      = "oauth2"
+	AuthPluginClassOAuth2 = "org.apache.pulsar.client.impl.auth.oauth2.AuthenticationOAuth2"
+	AuthPluginToken       = "token"
+	AuthPluginClassToken  = "org.apache.pulsar.client.impl.auth.AuthenticationToken"
+)
+
+// AuthProvider is a function that receives a default HTTP transport and returns
+// a new transport that handles auth. Implementations should remember to clone
+// any requests before modifying them.
+type AuthProvider func(*http.Transport) (http.RoundTripper, error)
+
+// AuthProviderPlugin returns an AuthProvider that will inspect a
+// Pulsar auth plugin/class name and JSON params and initialize the appropriate
+// authentication provider
+func AuthProviderPlugin(pluginName string, authParams string) AuthProvider {
+	return func(transport *http.Transport) (http.RoundTripper, error) {
+		switch pluginName {
+		case AuthPluginTLS, AuthPluginClassTLS:
+			params := &AuthParamsTLS{}
+			if seemsJSON(authParams) {
+				if err := unmarshalParams(authParams, params); err != nil {
+					return nil, err
+				}
+				return auth.NewTLSProvider(params.TLSCertFile, params.TLSKeyFile, transport)
+			}
+			// also supports KV
+			return auth.NewTLSProviderFromKV(authParams, transport)
+		case AuthPluginToken, AuthPluginClassToken:
+			params := &AuthParamsToken{}
+			if seemsJSON(authParams) {
+				if err := unmarshalParams(authParams, params); err != nil {
+					return nil, err
+				}
+				return auth.NewTokenProvider(params.Token, transport)
+			}
+			// also supports KV
+			return auth.NewTokenProviderFromKV(authParams, transport)
+		case AuthPluginOAuth2, AuthPluginClassOAuth2:
+			params := &AuthParamsOAuth2{}
+			if err := unmarshalParams(authParams, params); err != nil {
+				return nil, err
+			}
+			return auth.NewOauth2Provider(params.IssuerURL, params.Audience,
+				params.Scope, params.ClientID, params.PrivateKey, transport)
+		}
+		return nil, fmt.Errorf("unknown AuthPlugin %q", pluginName)
+	}
+}
+
+// AuthParamsTLS represents the JSON data needed to initialize a client with
+// AuthProviderPlugin and TLS authentication.
+//
+// It can also be provided as a string in the format of:
+//
+//	<key>:<path>,<key>:<path>
+//
+//	ex: "tlsCertFile:/path/to/cert,tlsKeyFile:/path/to/key"
+type AuthParamsTLS struct {
+	TLSCertFile string `json:"tlsCertFile"`
+	TLSKeyFile  string `json:"tlsKeyFile"`
+}
+
+// AuthParamsOAuth2 represents the JSON data needed to initialize a client with
+// OAuth2 Authentication
+type AuthParamsOAuth2 struct {
+	IssuerURL  string `json:"issuerUrl,omitempty"`
+	ClientID   string `json:"clientId,omitempty"`
+	Audience   string `json:"audience,omitempty"`
+	Scope      string `json:"scope,omitempty"`
+	PrivateKey string `json:"privateKey,omitempty"`
+}
+
+// AuthParamsToken represents the JSON data needed to initialize a client with
+// AuthProviderPlugin and Token auth.
+//
+// It can also be provided as a string in the format of:
+//
+//	token:<base64-encoded JWT token>
+//	or
+//	file:<path to encoded JWT token file>
+type AuthParamsToken struct {
+	Token string `json:"token"`
+}
+
+// AuthProviderToken returns a token-based authentication provider from raw
+// token data.
+func AuthProviderToken(token string) AuthProvider {
+	return func(transport *http.Transport) (http.RoundTripper, error) {
+		return auth.NewTokenProvider(token, transport)
+	}
+}
+
+// AuthProviderTokenFile returns a token-based authentication provider from a
+// token stored in a file.
+func AuthProviderTokenFile(tokenPath string) AuthProvider {
+	return func(transport *http.Transport) (http.RoundTripper, error) {
+		return auth.NewTokenProviderFromFile(tokenPath, transport)
+	}
+}
+
+func unmarshalParams(jsonStr string, v any) error {
+	return json.Unmarshal([]byte(jsonStr), v)
+}
+
+func seemsJSON(str string) bool {
+	if len(str) > 0 && str[0] == '{' {
+		return true
+	}
+	return false
+}
---
+++ internal/auth/token.go
@@ -0,0 +1,78 @@
+// Copyright 2023 StreamNative, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package auth
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/pkg/errors"
+
+	"github.com/streamnative/pulsar-admin-go/internal/httptools"
+)
+
+const (
+	tokenPrefix = "token:"
+	filePrefix  = "file:"
+)
+
+type TokenTransport struct {
+	Token string
+	T     http.RoundTripper
+}
+
+func NewTokenProvider(tokenData string, transport *http.Transport) (*TokenTransport, error) {
+	tokenData = strings.TrimSpace(tokenData)
+	if len(tokenData) == 0 {
+		return nil, errors.New("empty token")
+	}
+	return &TokenTransport{
+		Token: tokenData,
+		T:     transport,
+	}, nil
+}
+
+func NewTokenProviderFromKV(paramString string, transport *http.Transport) (*TokenTransport, error) {
+	switch {
+	case strings.HasPrefix(paramString, tokenPrefix):
+		token := paramString[len(tokenPrefix):]
+		return NewTokenProvider(token, transport)
+	case strings.HasPrefix(paramString, filePrefix):
+		tokenFile := paramString[len(filePrefix):]
+		return NewTokenProviderFromFile(tokenFile, transport)
+	default:
+		return NewTokenProvider(paramString, transport)
+	}
+}
+
+func NewTokenProviderFromFile(tokenPath string, transport *http.Transport) (*TokenTransport, error) {
+	fileContents, err := os.ReadFile(tokenPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, errors.New("token file not found")
+		}
+		return nil, fmt.Errorf("unable to open token file: %v", err)
+	}
+	return NewTokenProvider(string(fileContents), transport)
+}
+
+func (p *TokenTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	req = httptools.CloneReq(req)
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", p.Token))
+	return p.T.RoundTrip(req)
+}
